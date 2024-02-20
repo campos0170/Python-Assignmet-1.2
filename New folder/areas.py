5a2258f8a6ba79cdb2971bb5f876dc68ebebfb27
unknown <campos0170@gmail.com>
2024-02-19 22:57:57 -0500

    Python Basics 1

---
+++ New folder/areas.py
@@ -0,0 +1,17 @@
+from math import pi
+
+def area(lenght_,width_): 
+    """
+    Takes two parameters and returns their product
+
+
+    """
+    return lenght_*width_
+
+def circumference_(radius_):
+    """
+    takes the radius of a circle as input and returns its area
+    """
+    return 2*pi*radius_
+
+
